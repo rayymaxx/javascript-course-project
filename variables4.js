f20a7d4ad4753a9722b54b912b76bcac9313daf2
rayymaxx <raymax461@gmail.com>
2025-05-24 16:42:10 +0300

    Moved the object code to another file

---
+++ variables4.js
@@ -0,0 +1,17 @@
+let course = {
+    name: 'Javascript for beginners',
+    hours:3
+};
+// console.log(course.name);
+
+// course.name = 'Javascript Fundamentals';
+// console.log(course.name);
+
+// console.log(course['name']);
+
+// course['name'] = 'Javascript 101';
+// console.log(course.name);
+
+let property = 'hours';
+
+console.log(course[property]);
