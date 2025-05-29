1557de7f8c1a1f3ee4ac9fade99bde5e93c376c3
rayymaxx <rayymondodhiambo@gmail.com>
2025-05-29 14:04:12 +0300

    Used equality operators

---
+++ javascript-operators/equality.js
@@ -0,0 +1,15 @@
+let a = 2;
+let b = '2';
+
+// /* Loose equality-compating number and string */
+console.log(a == b);
+// //Evaluates truthy
+
+
+// stripped equality- compares without conversions
+console.log(a === b);
+
+
+console.log(1 == '1');
+
+console.log(true == '1');
