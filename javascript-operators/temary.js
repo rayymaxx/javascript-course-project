1f66d9cd978390b60535d0d8d0b311331efcb122
rayymaxx <rayymondodhiambo@gmail.com>
2025-05-29 14:07:56 +0300

    Used temary operator

---
+++ javascript-operators/temary.js
@@ -0,0 +1,8 @@
+// let age = 16;
+// const canDrive = age >= 16 ? true : false;
+
+// console.log(canDrive);
+
+let points = 110;
+const customerType = points > 100 ? 'gold' : 'silver';
+console.log(customerType);
